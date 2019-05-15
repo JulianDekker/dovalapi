ec63eb41e8d2d65b1900238b12e0358062927a2d
JulianDekker <Julian.dekker@live.nl>
2019-05-15 15:58:08 +0200

    version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 files = ['lib/*']
 
 setup(name='dovalapi',
-      version='2.1.0',
+      version='2.2.0',
       description='Doval methods and tools',
       url='',
       author='Julian Dekker',
